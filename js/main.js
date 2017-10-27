608dae6b9027bd2afe5f2b7afe08ee6331f40645
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-10-26 22:55:08 -0600

    Added a method to return a game's status

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-var game = {
-    power: false,
-    score: 0,
-    pattern: [],
-    input: [],
-    valid: true
+function Game() {
+    this.power = false;
+    this.score = 0;
+    this.pattern = [];
+    this.userPattern = [];
+    this.nextPattern = function () {
+        //add another button press to the pattern
+    };
+    this.getStatus = function () {
+        var inputLength = this.userPattern.length;
+        var patLength = this.pattern.length;
+
+        if (inputLength > patLength) return 2;
+        for (var i = 0; i < inputLength - 1; i++) {
+            if (this.pattern[i] !== this.userPattern[i]) return 2
+        }
+        if (inputLength === patLength) return 1
+        return 0
+        // status returns one of:
+        //   - 0 user has not made a mistake yet
+        //   - 1 user has finished the pattern for this turn
+        //   - 2 user has made a mistake
+    };
+
 }
 
 var buttons = Array.prototype.slice.call(document.querySelectorAll('.button'));
