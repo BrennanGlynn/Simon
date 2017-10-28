d41ce2ca8a5d89e995ad1918e7d496c814279853
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-10-27 19:10:40 -0600

    Minimum playable game finished

--- js/main.js
+++ js/main.js
@@ -1,33 +1,78 @@
 function Game() {
     this.power = false;
-    this.score = 0;
+    this.score = -1;
     this.pattern = [];
     this.userPattern = [];
-    this.nextPattern = function () {
-        //add another button press to the pattern
+    this.powerOn = function () {
+        this.power = true;
+    };
+    this.playGame = function () {
+        if (this.power) {
+            switch (this.getStatus()) {
+                case -1:
+                    //end the game
+                    //endGame()
+                    console.log("end the game dickface");
+                    this.power = false;
+                    break;
+                case 0:
+                    //let the player continue
+                    break;
+                case 1:
+                    //proceed with the game
+                    console.log("please proceed!");
+                    console.log(g);
+                    this.score++;
+                    this.pattern.push(Math.floor(Math.random() * 4));
+                    this.userPattern = [];
+                    break;
+            }
+        }
+    };
+    this.userInsert = function (btnValue) {
+      this.userPattern.push(btnValue)
     };
     this.getStatus = function () {
-        var inputLength = this.userPattern.length;
-        var patLength = this.pattern.length;
+        var pattern = this.pattern;
+        var userPattern = this.userPattern;
+        var error = false;
 
-        if (inputLength > patLength) return 2;
-        for (var i = 0; i < inputLength - 1; i++) {
-            if (this.pattern[i] !== this.userPattern[i]) return 2
+        if (userPattern.length > pattern.length) {
+            error = true;
         }
-        if (inputLength === patLength) return 1
-        return 0
-        // status returns one of:
-        //   - 0 user has not made a mistake yet
-        //   - 1 user has finished the pattern for this turn
-        //   - 2 user has made a mistake
-    };
 
+        userPattern.forEach(function (val, i) {
+            if (val !== pattern[i]) {
+                error = true;
+            }
+        });
+
+        if (error) {
+            return -1;
+        } else if (userPattern.length < pattern.length) {
+            return 0;
+        } else {
+            return 1;
+        }
+    };
 }
+
+var g = new Game();
+g.powerOn();
+
+function x() {
+    g.playGame();
+}
+
+setInterval(x, 100);
+
 
 var buttons = Array.prototype.slice.call(document.querySelectorAll('.button'));
 
 buttons.forEach(function (element) {
     element.addEventListener('click', function () {
-        console.log(this.getAttribute('value'))
+        var value = parseInt(this.getAttribute('value'));
+        g.userInsert(value);
+        console.log("User inserted " + value);
     })
 })
